c76ad950eaef351b09011505e6ff08f9f1286b47
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-01-19 23:45:55 +0100

    project setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        pink: "hsl(322, 100%, 66%)",
+        lightPink: "hsl(321, 100%, 78%)",
+        lightRed: "hsl(0, 100%, 63%)",
+        veryDarkCyan: "hsl(192, 100%, 9%)",
+        veryPaleBlue: "hsl(207, 100%, 98%)"
+      },
+      screens:{
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+      }
+    },
   },
   plugins: [],
 }
