c624c50d6d8a7c4ebd0ae555ad2ae700e4d65d5f
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-01-20 16:49:50 +0100

    users done

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import imgConversation from "../assets/illustration-flowing-conversation.svg";
 
 const Conversations = () => {
   return (
-    <div>Conversations</div>
-  )
-}
+    <section className="flex flex-col items-center lg:mx-[120px] my-[100px] lg:mt-[120px] lg:flex-row lg:justify-between">
+      <div className="image mb-[80px] lg:mb-0 lg:basis-[45%]">
+        <img className="w-[280px] lg:w-[530px]" src={imgConversation} alt="conversation_image" />
+      </div>
+      <div className="text lg:basis-[45%] text-center lg:text-left flex flex-col justify-center">
+        <h2 className="text-[20px] lg:text-[40px] mb-[26px]">Flowing Conversations</h2>
+        <p className="text-veryDarkCyan text-base hidden lg:block">
+          You wouldn't paginate a conversation in real life, so why do it
+          online? <br /> Our threads have just-in-time loading for a more natural flow.
+        </p>
+        <p className="text-veryDarkCyan text-[14px] lg:hidden px-4">
+          You wouldn't paginate a conversation in real life, so why do it
+          online? Our threads have just-in-time loading for a more natural flow.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Conversations;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import imgTopDesktop from "../assets/bg-section-top-desktop-1.svg";
+import imgTopMobile from "../assets/bg-section-top-mobile-1.svg";
+import imgBottomDesktop from "../assets/bg-section-bottom-desktop-1.svg";
+import imgBottomMobile from "../assets/bg-section-bottom-mobile-1.svg";
+import imgGrowTogether from "../assets/illustration-your-users.svg";
 
 const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <>
+      <section className="users relative hidden lg:block">
+        <div>
+          <img
+            className="w-full h-[421px]"
+            src={imgTopDesktop}
+            alt="section_image"
+          />
+          <img
+            className="w-full h-[421px]"
+            src={imgBottomDesktop}
+            alt="section_image"
+          />
+        </div>
+        <div className="flex w-[1200px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <div className="text basis-1/2 flex flex-col justify-center">
+            <h2 className="text-[40px] mb-[26px]">Your Users</h2>
+            <p className="text-veryDarkCyan">
+              It takes no time at all to integrate Huddle with your app's <br />
+              authentication solution. This means, once signed in to your <br /> app,
+              your users can start chatting immediately.
+            </p>
+          </div>
+          <div className="image basis-1/2">
+            <img
+              className="w-[560px]"
+              src={imgGrowTogether}
+              alt="grow_together"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="users relative lg:hidden mt-[-100px]">
+        <div>
+          <img
+            className="w-full h-[421px]"
+            src={imgTopMobile}
+            alt="section_image"
+          />
+          <img
+            className="w-full h-[421px]"
+            src={imgBottomMobile}
+            alt="section_image"
+          />
+        </div>
+        <div className="flex flex-col-reverse absolute top-[50%] translate-y-[-50%]">
+          <div className="text justify-center text-center borde-6">
+            <h2 className="text-[20px] mt-[40px]">Your Users</h2>
+            <p className="text-veryDarkCyan mt-[26px] text-[14px] px-4">
+              It takes no time at all to integrate Huddle with your app's
+              authentication solution. This means, once signed in to your app,
+              your users can start chatting immediately.
+            </p>
+          </div>
+          <div className="image mx-auto">
+            <img
+              className="w-[280px]"
+              src={imgGrowTogether}
+              alt="grow_together"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Users;
